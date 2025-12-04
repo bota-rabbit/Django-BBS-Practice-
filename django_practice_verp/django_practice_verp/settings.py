d5c0324b2c0f088d6c90002a7d1c7985135a082f
bota-rabbit <rabbit_one75@yahoo.co.jp>
2025-12-04 19:57:32 +0900

    Update settings.py

--- django_practice_verp/django_practice_verp/settings.py
+++ django_practice_verp/django_practice_verp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '↑ ここに新しく生成したキーを入れてください'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,4 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
